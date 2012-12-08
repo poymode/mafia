924595b3b1dcc62c01f6347bc762e9287091c276
Jan Mendoza <poymode@gmail.com>
2012-12-08 20:36:19 +0800

    converted all views to haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Mafia::Application.routes.draw do
   resources :roles
-
-
   resources :users
+  resources :games
 
 
   # The priority is based upon order of creation:
@@ -54,7 +53,7 @@ Mafia::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root :to => 'users#index'
 
   # See how all your routes lay out with "rake routes"
 
