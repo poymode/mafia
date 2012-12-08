cddeb9212da0b65b5ef0e234fda26a92c5798036
Jan Mendoza <poymode@gmail.com>
2012-12-08 19:58:13 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mafia::Application.config.secret_token = '95ace0e282c8e4c8e01028c1a7f2d86977d6cc7bb64faab2203f3821a29acbab6b5961f49f6c4275d3404381c4df6c51e19053a7c3039c449f13970f6038ceb2'
